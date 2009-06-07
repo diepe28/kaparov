85a11b9d60512a149d31f0f56b72a8177aa1d286
dsolistorres <dsolistorres@3df3d076-22cb-11de-b44d-8590aa00aed0>
2009-06-07 23:33:53 +0000

    Progra 2

---
+++ trunk/Progra2/http/ProtocoloHttp.h
@@ -0,0 +1,39 @@
+#ifndef PROTOCOLO_HTTP_H
+#define PROTOCOLO_HTTP_H
+
+typedef struct {
+    int idSocket;
+} ServidorHttp;
+
+typedef enum {
+    OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+} MetodoHttp;
+
+typedef struct {
+    MetodoHttp metodo;
+    char * url;
+    int versionMayor;
+    int versionMenor;
+} SolicitudHttp;
+
+typedef enum {
+    OK, NOT_FOUND
+} CodigoErrorHttp;
+
+typedef struct {
+    int versionMayor;
+    int versionMenor;
+    CodigoErrorHttp codigoError;
+    char * descripcionError;
+
+    int longitudMensaje;
+    char * mensaje;
+} RespuestaHttp;
+
+typedef RespuestaHttp * (*ManejadorHttp)(SolicitudHttp *);
+
+ServidorHttp * crearServidorHttp(short puerto, int maxSolicitudes);
+void finalizarServidorHttp(ServidorHttp * servidor);
+int aceptarSolicitudHttp(ServidorHttp * servidor, ManejadorHttp manejador);
+
+#endif // PROTOCOLO_HTTP_H
